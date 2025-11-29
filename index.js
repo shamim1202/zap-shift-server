711d5f54757ca0d81fcb5170afbb2e1d781d9af6
shamim1202 <mehedi.farzu@gmail.com>
2025-11-30 01:16:43 +0600

    create api for intregrate international payment gateway -stripe- for parcel payment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const app = express();
 
 // Middleware ----------------------------------------->
@@ -27,20 +28,28 @@ async function run() {
     const db = client.db("zap-shift");
     const parcelCollection = db.collection("parcels");
 
+    // Get parcel by email api cal ----------------------|
     app.get("/parcels", async (req, res) => {
       const query = {};
       const { email } = req.query;
       if (email) {
         query.senderEmail = email;
       }
-
       const options = { sort: { createdAt: -1 } };
-
       const cursor = parcelCollection.find(query, options);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // Paid a parcel with id  by get id api call --------|
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Add parcel by post api call ---------------------|
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       parcel.createdAt = new Date();
@@ -48,11 +57,87 @@ async function run() {
       res.send(result);
     });
 
+    // Delete a parcel by delete api call -----------------|
     app.delete("/parcels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await parcelCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // Payment Gateway International (Stripe) Api -----------|
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      console.log(session);
+      res.send({ url: session.url });
+    });
+
+    // Payment Gateway International (Stripe) Api (Alt) -----------|
+    app.post("/payment-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: `Please pay for ${paymentInfo.parcelName}`,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      res.send({ url: session.url });
+    });
+
+    app.patch("/payment-success", async (req, res) => {
+      const sessionId = req.query.session_id;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      if (session.payment_status === "paid") {
+        const id = session.metadata.parcelId;
+        const query = { _id: new ObjectId(id) };
+        const update = {
+          $set: {
+            paymentStatus: "paid",
+          },
+        };
+        const result = await parcelCollection.updateOne(query, update);
+        res.send(result);
+      }
+
+      res.send({ success: false });
     });
 
     await client.db("admin").command({ ping: 1 });
